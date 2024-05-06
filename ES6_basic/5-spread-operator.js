401735ce36047405fe298176396ca70b9569f40d
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-06 13:18:25 -0500

    spread syntax to concatenate arrays/strings

---
+++ ES6_basic/5-spread-operator.js
@@ -0,0 +1,4 @@
+export default function concatArrays(array1, array2, string) {
+  // spread syntax takes all elements of each object and combines them
+  return (...array1, ...array2, ...string);
+}
