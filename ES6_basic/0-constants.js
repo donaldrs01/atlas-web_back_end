0db6691f37b5c153ea19ce8b2916fed2f9330c58
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-06 10:52:39 -0500

    ESLint fixes?

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,17 +1,17 @@
 export function taskFirst() {
-    // ensures that it's a consistent value that doesn't change
-    const task = 'I prefer const when I can.';
-    return task;
+  // ensures that it's a consistent value that doesn't change
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
 export function getLast() {
-    return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-    // because the value of 'combination' is changed, we use l3t keyword
-    let combination = 'But sometimes let';
-    combination += getLast();
+  // because the value of 'combination' is changed, we use l3t keyword
+  let combination = 'But sometimes let';
+  combination += getLast();
 
-    return combination;
+  return combination;
 }
