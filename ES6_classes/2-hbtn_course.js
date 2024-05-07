f2bfc74c288ad3b55b9e0ad7c6063f694fbc28e8
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 12:44:19 -0500

    first pass at more detailed class construction with getters/setters

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,38 @@
+import ClassRoom from "./0-classroom";
+
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name; // call 'name' setter for type validation
+    this._length = length;
+    this.students = students;
+  }
+  // Getter / setter methods for 'name' property
+  get name() {
+    return this._name;
+  }
+  set name(nameValue) {
+    if (typeof nameValue !== 'string') {
+        throw new TypeError('Name must be a string');
+    }
+    this._name = nameValue;
+  }
+
+  // Getter / setter methods for 'length' property
+  get length() {
+    return this._length;
+  }
+  set length(lengthValue) {
+    if (typeof lengthValue !== 'number') {
+        throw new TypeError('Length must be a number');
+    }
+    this._length = lengthValue;
+  }
+
+  // Getter / setter methods for 'students' property
+  get students() {
+    return this._students;
+  }
+  set students(studentsValue) {
+    this._students = studentsValue;
+    }
+}
