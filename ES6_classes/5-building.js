289b0c066a7f8d90ba94d890b94a2f63ca97dda3
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 13:36:08 -0500

    abstract class creation

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,7 @@ class Building {
   constructor(sqft) {
     // Abstract class - issues warning if not an instance of Building class
     if (this.constructor !== Building && !this.evacuationWarningMessage) {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
