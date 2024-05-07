6286c29721c51c6ba303747bd7ba85eff4937da4
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 12:20:16 -0500

    classroom instance creation

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,15 @@
+import ClassRoom from './0-classroom';
+
+function initializeRooms() {
+  const rooms = [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
+
+    return rooms;
+
+}
+
+// Export function to allow for import elsewhere
+export {initializeRooms};
