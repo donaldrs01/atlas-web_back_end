824463072511a8e01aab85845ca905b7aef47c8a
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 13:48:17 -0500

    class inheritance

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,7 +3,11 @@ import Building from './5-building';
 class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft); // Calls parent class constructor for sqft attribute
-    this._floors = floors;  
+    this._floors = floors;
+  }
+
+  get sqft() { // Inherited from Building - allows for read access to sqft
+    return this._sqft;
   }
 
   get floors() {
