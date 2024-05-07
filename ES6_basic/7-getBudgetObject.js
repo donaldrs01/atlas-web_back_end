9e9278a9715bf93f4f487585ac0392495c2e51ca
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 06:37:16 -0500

    property value shorthand

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,10 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    // shorthand syntax allows for property and variable names to be identical
     income,
     gdp,
     capita,
-  };
+};
 
   return budget;
 }
+  // shorthand syntax allows for property and variable names to be identical
