f032cd8d085dfb1dd0adb04865825dbca6740b50
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 22:56:28 -0500

    reduce function to accumulate sum of student ids

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,6 +2,6 @@ export default function getStudentIdsSum(list) {
   if (!Array.isArray(list)) {
     return []; // return empty array if list not an array
   }
-  return list.reduce((sum, student) => sum + student.id);
-  // sum holds current value
+  return list.reduce((sum, student) => sum + student.id, 0);
+  // sum holds current value and initial value set to 0
 }
