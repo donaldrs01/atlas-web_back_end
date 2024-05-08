48d34296b2078684f5afcea5c9c3e15b336a8453
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 14:35:26 -0500

    edits to try and correctly access photo and user objects

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -4,6 +4,7 @@ export default function handleProfileSignup() {
   // Receive all promise requests from uploadPhoto / createUser
   return Promise.all([uploadPhoto(), createUser()])
     .then((resolvedPromises) => {
+      // Extract resolved values from photo/user objects and put them into array
       const [photoObject, userObject] = resolvedPromises;
       console.log(`${photoObject.body} ${userObject.firstName} ${userObject.lastName}`);
     })
