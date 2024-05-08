c1245141cfedb0266e75b4bc2596f4d09c4d4a93
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 11:33:50 -0500

    update new map with values re-set to 100

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  // create new map to return with updated values
+  const updatedMap = new Map();
+
+  for (const [key, value] of map) {
+    if (value === 1) {
+      // update value to 100 if set to 1
+      updatedMap.set(key, 100);
+    } else {
+      updatedMap.set(key, value);
+    }
+  }
+  return updatedMap;
+}
