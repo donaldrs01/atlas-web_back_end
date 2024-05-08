f7f9432c4ff20fe26071a08c61e1ff7646e33048
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 10:48:33 -0500

    create typed int8 array buffer to hold binary data at specified location

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Requested position outside of array range.');
+  }
+  // create new arrayBuffer obj with given length
+  const arrayBuffer = new ArrayBuffer(length);
+  // create Int-8 type array out of ArrayBuffer
+  const int8Array = new Int8Array(arrayBuffer);
+  // set correct value
+  int8Array[position] = value;
+
+  return arrayBuffer;
+}
