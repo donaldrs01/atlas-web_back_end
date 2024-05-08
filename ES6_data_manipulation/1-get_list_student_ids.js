cf055cdb323dbfe1ff5369e1aa03f954aa596983
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-07 22:21:48 -0500

    map function to extract student ids

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,5 +2,5 @@ export default function getListStudentIds(list) {
   if (!Array.isArray(list)) {
     return []; // return empty array if list not an array
   }
-  return list.map(student => student.id); // extract ID property from each student object in array
+  return list.map((student) => student.id); // extract ID property from each student object in array
 }
