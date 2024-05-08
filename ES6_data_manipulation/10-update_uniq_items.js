0b22c22fec191d45652389e4372ff2f4415b8890
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 11:44:56 -0500

    needed to return original map

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -3,15 +3,15 @@ export default function updateUniqueItems(map) {
     throw new Error('Cannot process');
   }
   // create new map to return with updated values
-  const updatedMap = new Map();
+  // const updatedMap = new Map();
 
   for (const [key, value] of map) {
     if (value === 1) {
       // update value to 100 if originally set to 1
-      updatedMap.set(key, 100);
+      map.set(key, 100);
     } else {
-      updatedMap.set(key, value);
+      map.set(key, value);
     }
   }
-  return updatedMap;
+  return map;
 }
