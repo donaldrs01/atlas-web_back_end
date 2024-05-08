cf1ed7be9e3815711b35aeac3f56ed26286bf189
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 13:26:59 -0500

    promise that resolves/rejects with log message

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  // Create new Promise that executes depending on resolution/failure of asyncronous operation
+  return new Promise((resolve, reject) => {
+    if (success) {
+      // On success, resolve with log message
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // Reject promise with error message
+      reject(new Error('The fake API is not working currently'));
+      }
+    });
+}
