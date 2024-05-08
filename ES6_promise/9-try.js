97bd9437085de77048d49a22000e4d3451576dc2
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 16:08:10 -0500

    guardrail that logs error when encountered, otherwise performs operation - prints guardrail message in all cases

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+export default function guardrail(mathFunction) {
+  // create empty array
+  const queue = [];
+  try {
+    // perform math operation and append the answer to the queue array
+    const answer = mathFunction();
+    queue.push(answer);
+  } catch (error) {
+    queue.push(error);
+  }
+  // Outside of this block, append guardrail message
+  // Will log in every case
+  queue.push('Guardrail was processed');
+  // Return the queue will all messages stacked
+  return queue;
+}
