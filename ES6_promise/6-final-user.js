61425d5151e9d04490e073e00a710cb5e9c62cd7
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 15:39:57 -0500

    starting task 6

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,6 @@
+import { signupUser } from './4-user-promise';
+import { uploadPhoto } from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  
+}
