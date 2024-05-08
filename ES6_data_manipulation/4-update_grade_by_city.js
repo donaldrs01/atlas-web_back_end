a7e8b9aa3cc4a3f2a829c2e08343e025b811b4fa
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 10:34:54 -0500

    filter by location and pull new grade into student object

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -4,7 +4,7 @@ export default function updateStudentGradeByCity(list, city, newGrades) {
   const updatedStudents = filteredStudents.map((student) => {
     const newGrade = newGrades.find((grade) => grade.studentId === student.id);
     if (newGrade) {
-      return { ...student, grade: newGrade.grade }
+      return { ...student, grade: newGrade.grade };
       // Spread syntax to create new student obj with newGrade
     }
     return { ...student, grade: 'N/A' }; // Set grade to 'N/A' if grade empty
