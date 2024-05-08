28a90a04de9f4af69aa394e9b28443cc0b2c70b1
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-05-08 14:17:46 -0500

    trying to resolve promises from uploadPhoto() and createUser()

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,11 +2,12 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   // Receive all promise requests from uploadPhoto / createUser
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((resolvedPromises) => {
       const photoObject = resolvedPromises[0];
       const userObject = resolvedPromises[1];
       console.log(`body ${userObject.firstName} ${userObject.lastName}`);
+      return { photoObject, userObject };
     })
     .catch((error) => {
       console.log('Signup system offline', error);
