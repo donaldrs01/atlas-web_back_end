233dfb0e7095865aaadd89a36985eadb6e12d82b
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 10:08:01 -0500

    type-switch int from float to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module that includes function definition for 'to_str'
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float 'n'
+
+    Args:
+        n (float): the float int
+
+    Returns:
+        str: the str representation of the int
+    """
+    return str(n)
