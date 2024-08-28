da43c7ce3d5de36d9b22a4ff493907b906b3ef52
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 09:57:59 -0500

    documentation edit

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,7 +5,7 @@ Module that defines a basic add function with type documentation
 
 
 def add(a: float, b: float) -> float:
-    """_summary_
+    """
 
     Args:
         a (float): float int
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module that includes a type-annotated function that concatenates two strings
+"""
+def concat(str1: str, str2: str) -> str:
+    """
+
+    Args:
+        str1 (string): first string
+        str2 (string): second string
+
+    Returns:
+        string: the concatenated string
+    """
