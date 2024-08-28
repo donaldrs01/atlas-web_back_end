b082a7dcae6cd5465d52fa7886ea4daffe3ee756
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 10:23:41 -0500

    task 5: use List type annotation to take list of input args and return sum as float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that includes function definition for 'sum_list'
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float: # type List
+    """
+    Function that receives series of floats as args and returns their sum
+
+    Args:
+        input_list (float): a list of float ints
+
+    Returns:
+        float: sum of ints as a float
+    """
+    return sum(input_list)
