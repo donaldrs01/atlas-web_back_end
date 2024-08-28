16843c37aa35e8087bcc5282f64ab27258b8ab28
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 09:54:55 -0500

    i forgot how to do documentation hehe

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+Module that defines a basic add function with type documentation
+"""
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): float int
+        b (float): float int
+
+    Returns:
+        float: float int
+    """
     return a + b
