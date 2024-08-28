ffd0d956d89250342edd1acb95b77c36cc50f616
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 10:12:09 -0500

    variable type definitions

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Module that includes a series of defined and annotated variables
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
