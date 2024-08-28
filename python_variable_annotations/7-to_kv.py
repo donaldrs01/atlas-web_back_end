30b8012e45d81efe3b6e13ed4243689053f9a08f
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-08-28 11:15:23 -0500

    task 7: combine str/int values in tuple with type annotations

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,3 +2,18 @@
 """
 Module that includes function definition for "to_kv"
 """
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Function that takes string and either int/float and returns combo of args
+
+    Args:
+        k (str): the original str value
+        v (Union[int, float]): num value (either int or float)
+
+    Returns:
+        Tuple[str, float]: combo of k/v values
+    """
+    return (k, float(v ** 2))  # square value of v
