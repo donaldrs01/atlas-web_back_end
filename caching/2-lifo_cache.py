6b2390c1a739ace097e85a05228a1f9a49356270
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-05 11:31:42 -0500

    task 2: LIFO cache system that pops most recently added element from cache list

---
+++ caching/2-lifo_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+LIFO Cache module implementation
+"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    Caching system that using LIFO (last in, first out)
+    and inherits properties from BaseCaching
+    """
+    def __init__(self):
+        """
+        Initialize cache system using LIFO logic
+        """
+        super().__init__()
+        self.key_stack = []  # initialize empty array to hold keys in order
+
+    def put(self, key, item):
+        """
+        Function to add items into cache.
+        Will remove first cached item if over limit
+
+        Args:
+            key: key that references the stored item
+            item: value associated with stored key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item  # add item to cache
+            self.key_stack.append(key)  # add item to key_stack array
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key = self.key_stack.pop()  # remove last element added
+            del self.cache_data[last_key]
+            print("DISCARD:", last_key)
+
+    def get(self, key):
+        """
+        Function to retrieve item based on key value
+
+        Args:
+            key: the key value of the item to be retrieved
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
