08dab9a7047a2c00ec186315437e219acb51d300
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-09 09:55:42 -0500

    task 0 : function to show start/end indexes based on page number and page size

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,3 +2,15 @@
 """
 Module for function index_range
 """
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Function that provides tuple providing start_index of page
+    and the end_index of the page
+    """
+    start_index = (page - 1) * page_size
+    #  start index defined by page number - 1 (0 index)
+    #  multiplied by the number of items on page
+    end_index = start_index + page_size
+    #  end index defined by + 10 from start_index of page
+    return (start_index, end_index)
