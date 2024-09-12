bbf019682833ee128a90a6d302df7c6277c0eb79
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-12 11:16:28 -0500

    task 0 - using regex to obfuscate certain information filtered into fields

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,3 +3,31 @@
 Module for filter_datum function that uses regex
 to obfuscate certain user information
 """
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """
+    Function that returns a log message with key information
+    obfuscated
+
+    Args:
+        fields (List[str]): list of strings containing all fields to obfuscate
+        redaction (str): str containing the symbol which will obfuscate PII
+        message (str): str that represents the log line
+        separator (str): str that represents the character that separates
+        each field in log line
+
+    Returns:
+        str: log message with certain fields obfuscated
+    """
+    # construct regex pattern to match field=value format
+    for field in fields:
+        #  re.escape(field) and re.escape(separator)
+        # treats each field string value and separator literally
+        pattern = rf"{re.escape(field)}=.*?{re.escape(separator)}"
+    #  re.sub to replace field values with redaction symbols
+        message = re.sub(pattern, f"{field}={redaction}{separator}", message)
+    return message
