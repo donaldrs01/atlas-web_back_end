5ac4965cbf06b8d9609241b0e4fb20985ca405c2
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-16 14:59:40 -0500

    using checkpw in bcrypt library to validate match between pw and hashed pw

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -22,3 +22,20 @@ def hash_password(password: str) -> bytes:
     hashed_pw = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
 
     return hashed_pw
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Function that verifies that hashed pw matches original pw
+
+    Args:
+        hashed_password (bytes) : the hashed version of the pw
+        password (str) : original password in plain text
+
+    Returns:
+        bool: True if match, False otherwise
+    """
+    # convert password to hashed version
+    bytes_pw = password.encode("utf-8")
+    # bcrypt validation that returns bool value
+    return bcrypt.checkpw(bytes_pw, hashed_password)
