0d8b89289446d0cd86782176237824d1ec9113b6
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-16 14:22:04 -0500

    task 5: using bcrypt to take password arg and return salted, hashed password

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module that contains hash_password function
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Function that hashes a password (passed as str)
+    using bcrypt
+
+    Args:
+        password (str) : the password to be hashed
+
+    Returns:
+        bytes: password that has been salted and hashed
+    """
+    # convert password to bytes
+    bytes_pw = password.encode("utf-8")
+    # hash the password with randomnly-generated salt
+    hashed_pw = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
+
+    return hashed_pw
