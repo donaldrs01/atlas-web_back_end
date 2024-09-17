9af98ca4752ae5307d9ea3905002eb3e650478e2
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-17 14:30:04 -0500

    fill out excluded_paths to specify which routes require authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -12,9 +12,26 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
-        Just returns false
+        Checks if path arg requires user authentication
+        - Returns True if path is None
+        - Returns True if excluded_paths arg is None or empty
+        - Returns False if path is on list of exclused_paths
         """
-        return False
+        if path is None:
+            return True
+        
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        # Slash tolerance
+        if not path.endswith('/'):
+            path += '/'
+        
+        if path in excluded_paths:
+            return False
+        # if none of these checks are met, require user auth
+        return True
+
 
     def authorization_header(self, request=None) -> str:
         """
