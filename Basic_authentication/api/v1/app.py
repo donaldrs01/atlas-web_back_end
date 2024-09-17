d8954feb2ba925230f1f9cfe3e7ea21ab1602498
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-17 14:39:49 -0500

    creating BasicAuth class and writing check for authentication type

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,12 +7,23 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+# Import Auth and BasicAuth classes
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+# Check for authentication type depending on AUTH_TYPE
+auth = None  # create auth instance
+AUTH_TYPE = getenv("AUTH_TYPE")
+
+if AUTH_TYPE == "basic_auth":
+    auth = BasicAuth()
+else:
+    auth = Auth()
 
 # Error handler for forbidden access (403) error
 @app.errorhandler(403)
---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for BasicAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth class
+    """
+    pass
