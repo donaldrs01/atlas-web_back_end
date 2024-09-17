84a6c58705adba227a532faaec5859ee2ffa0dfa
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-17 15:32:13 -0500

    decoding base64 auth header string into utf-8 format

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,3 +36,28 @@ class BasicAuth(Auth):
         # return the part after "Basic "
         # extracts substring from after the space
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """
+        Decodes Base64 part of auth header
+
+        Returns:
+        - None if base64 auth header is None
+        - None if base64 auth header is not a string
+        - None if base64 auth header is not a valid Base64 string
+        - On success, the decoded value as UTF-8 string
+        """
+        if base64_authorization_header is None:
+            return None
+        
+        if not isinstance(base64_authorization_header, str):
+            return None
+        
+        try:
+            # Decode Base64 string
+            decoded_string = base64.b64decode(base64_authorization_header)
+            # Convert into UTF-8 string
+            return decoded_string.decode('utf-8')
+        except (UnicodeDecodeError)
+            return None  # return None in case of decoding error
