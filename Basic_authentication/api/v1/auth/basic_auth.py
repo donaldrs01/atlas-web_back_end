99e07420aa36da7844acc9e129688e907c67d42c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-17 15:18:57 -0500

    Base64 string extraction of auth header

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,28 @@ class BasicAuth(Auth):
     BasicAuth class that inherits from Auth class
     """
     pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Function that extracts Base64 part of auth header
+
+        Returns:
+        - None if auth_header is None
+        - None if auth_header is not a string
+        - None if auth_header doesn't start with "basic"
+        - The Base64 part of "basic" otherwise
+        """
+        if authorization_header is None:
+            return None
+        
+        # check that authorization_header is string
+        if not isinstance(authorization_header, str):
+            return None
+        
+        # use 'startswith' to check beginning of string
+        if not authorization_header.startswith("Basic "):
+            return None
+        
+        # return the part after "Basic "
+        # extracts substring from after the space 
+        return authorization_header[len("Basic "):]
