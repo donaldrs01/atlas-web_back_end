d872940ace825a9025731775ace7fdd604a2295a
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-18 15:26:52 -0500

    session auth class

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for SessionAuth class
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Session authentication class who has parent Auth
+    """
+    pass
