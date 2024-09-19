ec4d2a4fb3ca136174a2b6c092d96e9039020648
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-19 10:48:42 -0500

    created new view for user/session authentication

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -8,6 +8,7 @@ from api.v1.views import app_views
 from models.user import User
 from os import getenv
 
+
 @app_views.route('/auth_session/login', methods=["POST"], strict_slashes=False)
 def user_login():
     """
@@ -16,7 +17,7 @@ def user_login():
     # pull user email/password from session request
     email = request.form.get("email")
     password = request.form.get("password")
-  
+
     # validate
     if not email:
         return jsonify({"error": "email missing"}), 400
@@ -29,7 +30,7 @@ def user_login():
     # check to make sure password matches the user
     if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
-   
+
     from api.v1.app import auth
     # create session ID for user
     session_id = auth.create_session(user.id)
@@ -40,7 +41,3 @@ def user_login():
     response = make_response(jsonify(json_user))
     # set session cookie
     response.set_cookie(getenv("SESSION_NAME"), session_id)
-
-    
-
-    
