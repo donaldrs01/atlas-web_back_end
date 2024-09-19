7095556da1370c28ed286920da9b47d5bc8bb74d
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-18 21:16:21 -0500

    adding new URL path (login) to be accessible without authentication credentials

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -44,14 +44,8 @@ def before_request() -> None:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)  # unauthorized error if no auth header returned
-    
-    if auth.session_cookie(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)  # forbidden access if user is not authorized
 
     # if current user not authenticated, abort with 403
     request.current_user = auth.current_user(request)
