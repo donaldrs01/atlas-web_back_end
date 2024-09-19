1a35c7f81059178c0511a82f554d63949a461da2
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-18 22:25:55 -0500

    current_user method to retrieve user based on session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 Module for SessionAuth class
 """
 from api.v1.auth.auth import Auth
+from models.user import User
 import uuid
 
 
@@ -44,3 +45,26 @@ class SessionAuth(Auth):
             return None
         # use .get() to retrieve user_id associated with given session id
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        Return user based on session ID retrieved from cookie
+        """
+        if request is None:
+            return None
+        
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        
+        try:
+            user = User.get(user_id)
+            if user is None:
+                return None
+            return user
+        except Exception:
+            return None
