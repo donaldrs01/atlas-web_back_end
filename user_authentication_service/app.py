faab6efbc9f4a183cc25608cdbd0b923ce8cb51c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 07:53:31 -0500

    task 7: adding /users route that registers user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,8 +3,10 @@
 Basic Flask application
 """
 from flask import Flask, jsonify
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -13,6 +15,20 @@ def welcome():
     GET route that returns JSON response
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """
+    Handles user registration
+    """
+    email = request.forn.get("email")
+    password = request.form.get("password")
+    try:
+        new_user = AUTH.register_user(email, password)
+        return jsonify({"email: f"{email}, "message" "user created"})
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
