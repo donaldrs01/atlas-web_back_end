ce2687d874406fe8c71fb8444e1dadbcb4ceb484
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 07:49:39 -0500

    task 6: creating flask app

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -6,6 +6,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def welcome():
     """
@@ -13,6 +14,6 @@ def welcome():
     """
     return jsonify({"message": "Bienvenue"})
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
-
