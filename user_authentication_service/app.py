1a8618f16b04b15fef7b4d5f7ec240761a73d77f
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 08:01:53 -0500

    changed error code

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -24,10 +24,11 @@ def users() -> str:
     """
     email = request.form.get("email")
     password = request.form.get("password")
-    
+
     try:
         new_user = AUTH.register_user(email, password)
-        return jsonify({"email": new_user.email, "message": "user created"}), 200
+        return jsonify({"email": new_user.email,
+                        "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
