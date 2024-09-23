bd7d0e2ba19d43813f0aa3774bd941e2f2cafb31
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 07:21:48 -0500

    task 2 - find user through filtered kwargs

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +40,17 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Function that returns first row of users table
+        filtered by input arguments
+        """
+        try:
+            find_user = self._session.query(User).filter_by(**kwargs).first()
+            if find_user is None:
+                raise NoResultFound
+            return find_user
+        except TypeError:
+            raise InvalidRequestError
+
