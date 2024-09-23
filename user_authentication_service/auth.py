c902710b4f8a699f9ca5a578bd3de626e0091228
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 08:19:33 -0500

    task 8: validating credentials with valid_login method that checks for hashed pw in storage

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -42,11 +42,12 @@ class Auth:
         # return False on any other exception or if password doesn't match
         return False
 
+
 def _hash_password(password: str) -> bytes:
-        """
-        Function that hashes password and returns in
-        byte representation
-        """
-        byte_password = password.encode("utf-8")
-        hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
-        return hashed_password
+    """
+    Function that hashes password and returns in
+    byte representation
+    """
+    byte_password = password.encode("utf-8")
+    hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+    return hashed_password
