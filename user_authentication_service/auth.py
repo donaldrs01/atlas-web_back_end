267b7d861e084a5ed7b36d0866452590866a4910
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-23 07:34:29 -0500

    task 5- creaitng/registering new user if email not saved

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -5,13 +5,37 @@ Module containig authentication logic
 from db import DB
 from user import Base, User
 import bcrypt
+from db import DB
 
 
-def _hash_password(password: str) -> bytes:
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    Function that hashes password and returns in
-    byte representation
-    """
-    byte_password = password.encode("utf-8")
-    hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
-    return hashed_password
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Class method that registers a user based on
+        email and pw input args
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+
+
+
+    def _hash_password(password: str) -> bytes:
+        """
+        Function that hashes password and returns in
+        byte representation
+        """
+        byte_password = password.encode("utf-8")
+        hashed_password = bcrypt.hashpw(byte_password, bcrypt.gensalt())
+        return hashed_password
+    
