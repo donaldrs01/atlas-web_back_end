7413395d97968396daa75585849dacb205ac16a9
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-09-25 07:35:35 -0500

    unittest for invalid inputs - checking to see that KeyError raised

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -14,4 +14,18 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """
+        Unittest for access_nested_map that validates expected values
+        """
         self.assertEqual(utils.access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),  # 'a' value missing test case
+        ({"a": 1}, ("a", "b")),  # 'b' value missing test case
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """
+        Unittest for access_nested_map that tests for invalid inputs
+        """
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
