875d41200d04c4dfa880f5337da6670d253d4631
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-10-03 11:50:39 -0500

    Delete MYSQL_Advanced directory

--- MYSQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- SQL script that creates new user table
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
