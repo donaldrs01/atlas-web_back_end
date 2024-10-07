0fc7486bef1f8c15bfb2e9ba9aeac965c9a232e4
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-07 12:10:00 -0500

    task 5: reset email attribute when user table is updated with new email

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,16 @@
+-- Task 5: Creating trigger to reset valid email attribute when email updated
+
+DELIMITER $$
+
+CREATE TRIGGER reset_valid_email
+BEFORE UPDATE ON users -- Trigger only activated before any updates on user table
+FOR EACH ROW
+BEGIN
+    -- Check if email has been updated
+    IF NEW.email <> OLD.email THEN
+        -- Reset attribute when changed
+        SET NEW.valid_email = 0;
+    END IF; -- Do nothing if new email same as old email
+END$$
+
+DELIMITER ;
