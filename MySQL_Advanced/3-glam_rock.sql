3adf68514f154fecf1b3ee727d24a87ddb58db6a
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-07 11:46:56 -0500

    filtering glam rock bands by lifespan

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,14 @@
+-- Task 3: Ordering glam rocks bands by lifespan
+
+SELECT
+    band_name,
+    CASE -- handle case where band still exists (split = NULL)
+        WHEN split IS NOT NULL THEN split - formed
+        ELSE YEAR(CURDATE()) - formed
+    END AS lifespan
+FROM
+    metal_bands
+WHERE 
+    style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
