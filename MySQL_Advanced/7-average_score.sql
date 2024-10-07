2130a4cbc53a79340e4550b3ac5772952f24cfba
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-07 12:44:35 -0500

    rename input value to avoid confusion

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -3,14 +3,14 @@
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id int
+    IN input_id int
 )
 BEGIN
     DECLARE avg_score FLOAT; -- declare avg_score variable
     -- Use corrections values to calculate average score and store in variable
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE user_id = input_id;
     -- Update average_score column in 'users' table with avg_score value
     UPDATE users
     SET average_score = avg_score
