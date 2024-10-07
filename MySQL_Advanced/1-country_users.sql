3bdf528bcdf13302ac96fa71caf353043bc0f86c
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-07 10:35:38 -0500

    syntax changes for ENUM

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,9 @@
 -- Task 1 : SQL script that creates table 'users' with enumeration constraints
 
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country enumeration("US", "CO", "TN") NOT NULL DEFAULT "US"
+    country ENUM("US", "CO", "TN") NOT NULL DEFAULT "US",
+    PRIMARY KEY (id)
 );
