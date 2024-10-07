9ee2f766f44d4aae117fe56ba7cbbbb3ee2e3acf
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-07 12:42:47 -0500

    finishing task 7: calculating, updating and storing student's average score

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id int
 )
 BEGIN
-    DECLARE avg_score FLOAT -- declare avg_score variable
+    DECLARE avg_score FLOAT; -- declare avg_score variable
     -- Use corrections values to calculate average score and store in variable
     SELECT AVG(score) INTO avg_score
     FROM corrections
@@ -14,7 +14,7 @@ BEGIN
     -- Update average_score column in 'users' table with avg_score value
     UPDATE users
     SET average_score = avg_score
-    WHERE id = user_id
+    WHERE id = user_id;
 END$$
 
 DELIMITER ;
