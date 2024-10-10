105dbd166b185aa21f54e3130d277d1f52b64053
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-10 11:47:30 -0500

    first part of task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,6 +11,13 @@ def log_stats():
     Function that performs various operations and displays Nginx logs
     """
     db = instance.logs
+    collection = db.nginx
 
-    document_count = db.nginx.count_documents({})
+    document_count = collection.count_documents({})
     print(f"{document_count} logs")
+
+    print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = 
+
