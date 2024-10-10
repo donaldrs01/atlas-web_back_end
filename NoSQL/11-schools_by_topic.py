8a56be13b32f869a4d091b8b5eb2e1129953e589
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-10 10:44:00 -0500

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module for function logic to return list with given topic attribute
+"""
+from pymongo import MongoClient
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Function that returns list of schools having a specific topic
+    """
+    school_list = []
+    for school in mongo_collection.find({topic: "topic"}):
+        school_list.append(school)
+    
+    return school_list
