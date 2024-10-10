35addb4569760bba9ccad8c3b67a1688037263b7
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-10 11:30:59 -0500

    first part of task 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -4,3 +4,11 @@ Module for function that displays log stats
 """
 from pymongo import MongoClient
 
+instance = MongoClient()
+
+def log_stats():
+    """
+    Function that performs various operations and displays Nginx logs
+    """
+    document_count = logs.nginx.count_()
+    print(f"{document_count} logs")
