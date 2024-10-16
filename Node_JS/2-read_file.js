92a272e2f7b5be1e56e0fd305187f147d963900f
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-16 12:40:56 -0500

    task 2 work

---
+++ Node_JS/2-read_file.js
@@ -0,0 +1,31 @@
+// countStudents function that reads CSV file, counts # of students, and logs to console
+// Implement fs module to read file
+const fs = require("fs");
+
+function countStudents(path) {
+    try {
+        const file_content = fs.readFileSync(path, "utf-8");
+        // Split content into lines and filter out empty lines
+        const lines = data.split("\n").filter(line => line.trim() !== "")
+        // Store student data in object array
+        const studentCounts = {};
+        let totalStudents = 0;
+        // Iterate through each line
+        for (let index = 0; index < lines.length; index++) {
+            const line = lines[index];
+            const data = line.split(",");
+            // Skip over header line and invalid data
+            if (index === 0 || data.length < 4) continue;
+            // Record valid student data
+            if (line !== "") {
+                const newStudent = {
+                    firstName: data[0].trim(), // Trim the whitespace
+                    lastName: data[1].trim(),
+                    age: data[2].trim(),
+                    field: data[3].trim(),
+                };
+            }
+        }
+
+
+}
