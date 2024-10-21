a809a4653a97ecbf70168f522fc546e057f87d7d
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-21 08:51:00 -0500

    finally finished task 5

--- Node_JS/5-http.js
+++ Node_JS/5-http.js
@@ -11,8 +11,9 @@ const app = http.createServer((req, res) => {
         const dbpath = process.argv[2]; // Grab db path from argument
         res.writeHead(200, {"Content-Type": "text/plain"});
         res.write("This is the list of our students:\n");
+        // Declare fileContent variable to hold student data
         // Read the CSV file using async logic
-        fs.readFile(dbpath, "utf-8", (err, data) => {
+        fs.readFile(dbpath, "utf-8", (err, fileContent) => {
             if (err) {
                 res.write("Cannot load the database");
                 res.end();
@@ -30,22 +31,36 @@ const app = http.createServer((req, res) => {
                 // Skip over header line and invalid data
                 if (index === 0 || data.length < 4) continue;
                 // Record valid student data
-                if (line !== "") {
-                    const newStudent = {
-                        firstName: data[0].trim(),
-                        lastName: data[1].trim(),
-                        age: data[2].trim(),
-                        field: data[3].trim(),
-                    };
-                    if (!studentCounts[newStudent.field]) {
-                        // Initialize new array to hold students if new field entry
-                        studentCounts[newStudent.field] = [];
-                    }
-                    studentCounts[newStudent.field].push(newStudent.firstName);
-                    totalStudents++
+                const newStudent = {
+                    firstName: data[0].trim(),
+                    lastName: data[1].trim(),
+                    age: data[2].trim(),
+                    field: data[3].trim(),
+                };
+                    
+                if (!studentCounts[newStudent.field]) {
+                    // Initialize new array to hold students if new field entry
+                    studentCounts[newStudent.field] = [];
                 }
-                
-                // Wri
+                studentCounts[newStudent.field].push(newStudent.firstName);
+                totalStudents++
+            }
+
+            res.write(`Number of students: ${totalStudents}\n`);
+
+            // Write number of students in each field
+            for (const field in studentCounts) {
+                const list = studentCounts[field].join(", ");
+                res.write(`Number of students in ${field}: ${studentCounts[field].length}. List: ${list}\n`);
+            }
+
+            res.end();
+        });
+    }
+});
+
+app.listen(1245, () => {
+    console.log("Server running on Port 1245");
 });
 
 module.exports = app;
