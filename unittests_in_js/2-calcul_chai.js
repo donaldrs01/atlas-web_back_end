4ffde84c155534cb5e93bcfec1d66a40f41fd6b7
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-22 11:57:31 -0500

    testing with chai

---
+++ unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,18 @@
+function calculateNumber(type, a, b) {
+    const Around = Math.round(a);
+    const Bround = Math.round(b);
+
+    // Perform operation based on type input
+    if (type === "SUM") {
+        return Around + Bround;
+    } else if (type === "SUBTRACT") {
+        return Around - Bround;
+    } else if (type === "DIVIDE") {
+        if (Bround === 0) {
+            return "Error";
+        }
+        return Around / Bround;
+    }
+}
+
+module.exports = calculateNumber;
