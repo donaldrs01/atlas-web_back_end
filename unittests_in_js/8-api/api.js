0ca44e47b8ce8fb12f5b7b43865f15cd546bc493
donaldrs01 <ryan.s.donaldson@gmail.com>
2024-10-23 13:52:00 -0500

    task 8: switched to supertest to try and appease checker

--- unittests_in_js/8-api/api.js
+++ unittests_in_js/8-api/api.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.send('Welcome to the payment system');
 });
 
+module.exports = app;
+
 app.listen(port, () => {
     console.log(`API available on localhost port ${port}`);
 });
--- unittests_in_js/8-api/api.test.js
+++ unittests_in_js/8-api/api.test.js
@@ -1,17 +1,28 @@
-const axios = require('axios');
+const request = require('supertest');
 const { expect } = require('chai');
+const app = require('./api');
+
 
 describe('API index page test', () => {
-    const url = 'http://localhost:7865/';
-
-    it('should return 200 status code', async () => {
-        // async/await to pause test execution until the get request finishes
-        const response = await axios.get(url);
-        expect(response.status).to.equal(200);
+    it('should return 200 status code', (done) => {
+        // Request to make GET request to app
+        request(app)
+            .get('/') // endpoint being tested
+            .expect(200) // status code to expect
+            .end((err, res) => {
+                if (err) return done(err); // fail test if error
+                done();
+            });
     });
 
-    it('should return the correct message', async () => {
-        const response = await axios.get(url);
-        expect(response.data).to.equal('Welcome to the payment system');
+    it('should return correct message', (done) => {
+        request(app)
+            .get('/')
+            .end((err, res) => {
+                if (err) return done(err);
+                // Expect test to verify response text matches expected output
+                expect(res.text).to.equal('Welcome to the payment system');
+                done();
+            });
     });
 });
